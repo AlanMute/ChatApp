485021586c5b7b174ef0cdb41ff42ea739775713
Mutalibov Alan <alanasia904@gmail.com>
2024-11-13 19:45:37 +0300

    fix adding chats

--- app/src/main/java/com/example/chatapp/models/AddChat.kt
+++ app/src/main/java/com/example/chatapp/models/AddChat.kt
@@ -1,7 +1,9 @@
 package com.example.chatapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class AddChat(
-    val name: String,
-    val isDirect: Boolean,
-    val membersIds: List<Int>
+    @SerializedName("name") val name: String,
+    @SerializedName("is_direct") val isDirect: Boolean,
+    @SerializedName("members_ids") val membersIds: List<Int>
 )
